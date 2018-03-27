70012db2039fba8b842b397f585948416a66818d
JonoAugustine <jaugust8@jhu.edu>
2018-03-27 14:32:12 -0400

    added android build

---
+++ AGZMob/Android_Multi/Install_AGZ_2_Development-armv7-es2.bat
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c98b6d8e27650aef28f70f3c2877ba1df79c67ae56c2680947f256d00f7575eb
+size 1640
---
+++ AGZMob/Android_Multi/Uninstall_AGZ_2_Development-armv7-es2.bat
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d769b7b6c9af60bb1c0712182a388b89e813137ad8921585d9894e44237ad154
+size 841
