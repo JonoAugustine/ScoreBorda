7e08c83501c5d7c571104c29d6a1ee127484010e
Jonathan Augustine <me@jonoaugustine.com>
2025-04-23 01:07:49 -0400

    feat: add Accordian component

---
+++ src/components/Accordian.tsx
@@ -0,0 +1,23 @@
+"use client"
+
+import { useState } from "react"
+import { HiMiniBarsArrowDown, HiMiniBarsArrowUp } from "react-icons/hi2"
+
+type AccordianProps = {
+  title: string
+  children: React.ReactNode
+}
+
+export default function Accordian({ title, children }: AccordianProps) {
+  const [open, setOpen] = useState(false)
+
+  return (
+    <div className="accordian">
+      <header onClick={() => setOpen(!open)}>
+        <p>{title}</p>
+        {open ? <HiMiniBarsArrowUp /> : <HiMiniBarsArrowDown />}
+      </header>
+      <div className="content" data-open={open}>{children}</div>
+    </div>
+  )
+}
